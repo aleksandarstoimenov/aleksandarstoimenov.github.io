e2a051c85d611b6ad11fd79f0bbdc8abf5bdff85
Aleksandar Stoimenov <alekstoimenov97@gmail.com>
2019-08-12 16:21:59 +0200

    eferf
    jnkf

--- Graphisme/script.js
+++ Graphisme/script.js
@@ -5,5 +5,50 @@ $(function (){
 	});
 });
 
+$(function (){
+	$(".foscup").click(function(){
+  	$(".bg-modal").toggleClass("on");
+	});
+});
 
+$(function (){
+	$(".croix1").click(function(){
+  	$(".bg-modal").toggleClass("on");
+	});
+});
 
+$(function (){
+	$(".chatsouris").click(function(){
+  	$(".bg-modal1").toggleClass("on");
+	});
+});
+
+$(function (){
+	$(".croix2").click(function(){
+  	$(".bg-modal1").toggleClass("on");
+	});
+});
+
+$(function (){
+	$(".couchsurfing").click(function(){
+  	$(".bg-modal2").toggleClass("on");
+	});
+});
+
+$(function (){
+	$(".croix3").click(function(){
+  	$(".bg-modal2").toggleClass("on");
+	});
+});
+
+$(function (){
+	$(".breuer").click(function(){
+  	$(".bg-modal3").toggleClass("on");
+	});
+});
+
+$(function (){
+	$(".croix4").click(function(){
+  	$(".bg-modal3").toggleClass("on");
+	});
+});
