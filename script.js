fa346ab51370b35be99c8ee638e51b7c06b3754c
aleksandarstoimenov <alekstoimenov97@gmail.com>
2024-02-18 22:16:38 +0100

    lol

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('start-button').addEventListener('click', function() {
-        document.getElementById('loading-screen').style.transform = 'translateY(100%)';
+        document.getElementById('loading-screen').style.transform = 'translateY(200%)';
     });
 
     document.getElementById('start-button-hovered').addEventListener('click', function() {
-        document.getElementById('loading-screen').style.transform = 'translateY(100%)';
+        document.getElementById('loading-screen').style.transform = 'translateY(200%)';
     });
-    
+
     document.getElementById('popup-cross').addEventListener('click', function() {
         document.getElementById('text-container-phone').style.transform = 'translate(300%, -50%)';
     });
